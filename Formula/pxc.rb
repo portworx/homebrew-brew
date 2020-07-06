0497a6114126b1b0361f120f0f8a1113afd93f32
Luis Pabón <luis@portworx.com>
2020-07-06 01:11:04 -0400

    use pxc public 0.28.0 release

--- Formula/pxc.rb
+++ Formula/pxc.rb
@@ -1,6 +1,6 @@
 class Pxc < Formula
   desc "Portworx Client"
-  url "file:///home/lpabon/git/golang/src/github.com/portworx/pxc/pxc-0.28.0.tar.gz"
+  url "https://github.com/portworx/pxc/archive/v0.28.0.tar.gz"
   sha256 "f3bf2d058a507fd0d64e41aea85ca6cdba994a4725ca1538976b3efef2ee641a"
   license "Apache-2.0"
   version "0.28.0"
