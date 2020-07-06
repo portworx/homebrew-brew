3201167cf1bf13bb335fcf351eed1cfa5316d896
Luis Pabón <luis@portworx.com>
2020-07-06 11:00:24 -0400

    Add unstable support

--- Formula/pxc-pxctl.rb
+++ Formula/pxc-pxctl.rb
@@ -4,6 +4,7 @@ class PxcPxctl < Formula
   url "https://github.com/portworx/pxc/archive/v0.28.0.tar.gz"
   sha256 "f3bf2d058a507fd0d64e41aea85ca6cdba994a4725ca1538976b3efef2ee641a"
   license "Apache-2.0"
+  head "https://github.com/portworx/pxc.git"
 
   depends_on "portworx/brew/pxc"
 
