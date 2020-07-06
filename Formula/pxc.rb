aade783a50c354224d360ec04d3ed271f53ceb1a
Luis Pabón <luis@portworx.com>
2020-07-06 01:13:44 -0400

    brew audit fixes

--- Formula/pxc.rb
+++ Formula/pxc.rb
@@ -1,9 +1,9 @@
 class Pxc < Formula
   desc "Portworx Client"
+  homepage "https://github.com/portworx/pxc"
   url "https://github.com/portworx/pxc/archive/v0.28.0.tar.gz"
   sha256 "f3bf2d058a507fd0d64e41aea85ca6cdba994a4725ca1538976b3efef2ee641a"
   license "Apache-2.0"
-  version "0.28.0"
 
   depends_on "go" => :build
 
