a9d0f6ebcbe42d8048b1bca338dc119c2ca56826
Luis Pabón <lpabon@purestorage.com>
2020-12-09 01:08:31 -0500

    Remove HEAD from pxc-pds

--- Formula/pxc-pds.rb
+++ Formula/pxc-pds.rb
@@ -4,7 +4,6 @@ class PxcPds < Formula
   url "https://github.com/portworx/homebrew-brew/releases/download/0.0.1/pxc-pds-v0.0.3-7-ge5aa647.tar.gz"
   sha256 "765632e29d9b07bc137245d32c33cf7cbc348a2d55b0da34e22b9fce842352e2"
   license "Apache-2.0"
-  head "https://github.com/portworx/pxc-pds.git"
 
   depends_on "portworx/brew/pxc"
   depends_on "helm"
