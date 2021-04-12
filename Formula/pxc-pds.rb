2ded6d05831eb8b1ccfccac2a0f371f4e98495ab
Piyush Nimbalkar <pnimbalkar@purestorage.com>
2021-04-11 18:33:15 -0700

    Update pxc-pds to 0.0.4
    Signed-off-by: Piyush Nimbalkar <pnimbalkar@purestorage.com>

--- Formula/pxc-pds.rb
+++ Formula/pxc-pds.rb
@@ -1,8 +1,8 @@
 class PxcPds < Formula
   desc "Portworx pxc component for Portworx Data Services (PDS)"
   homepage "https://github.com/portworx/pxc-pds"
-  url "https://github.com/portworx/homebrew-brew/releases/download/0.0.1/pxc-pds-v0.0.3-7-ge5aa647.tar.gz"
-  sha256 "765632e29d9b07bc137245d32c33cf7cbc348a2d55b0da34e22b9fce842352e2"
+  url "https://github.com/portworx/homebrew-brew/releases/download/0.0.1/pxc-pds-0.0.4.tar.gz"
+  sha256 "aec4d95fcecdef15c2c1820f72a4b84d0ee3d3654a5abba9611f5e3f9df45746"
   license "Apache-2.0"
 
   depends_on "portworx/brew/pxc"
